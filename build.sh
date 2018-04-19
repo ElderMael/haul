557a45720f03709b3121878433d8caf3d4180029
ElderMael <sftwr.mael@gmail.com>
2018-04-18 21:54:45 -0500

    Use Haul Compiled To Test

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ set -ex
 
 ./ceylonb run io.eldermael.haul --version
 
-PATH="$PATH:$(pwd)" ./ceylonb run io.eldermael.haul \
+./ceylonb assemble --include-language --out haul.jar io.eldermael.haul
+
+PATH="$PATH:$(pwd)" java -jar haul.jar \
           --repo https://github.com/ElderMael/haul_test_data.git \
           --verbose \
           --to-consul-cli \
           --to-etcd-cli \
           --to-stdout
-
-./ceylonb assemble --include-language --out haul.jar io.eldermael.haul
